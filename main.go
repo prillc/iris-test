e4aa3442c21d217b2bcf01d40bed93ae0d4b2817
陈品珍 <549471729@qq.com>
2021-01-28 15:19:55 +0800

    iris 路由和参数获取

--- main.go
+++ main.go
@@ -1,29 +1,50 @@
 package main
 
 import (
-	"github.com/kataras/iris/v12/mvc"
-	"iris-test/repositories"
-	"iris-test/services"
-	"iris-test/web/controllers"
+	"fmt"
 	"log"
+	"strconv"
 
 	"github.com/kataras/iris/v12"
 )
 
+func logThisMiddleware(ctx iris.Context) {
+	//ctx.Path() 请求的url
+	ctx.Application().Logger().Infof("Path: %s | IP: %s", ctx.Path(), ctx.RemoteAddr())
+
+	//ctx.Next 继续往下一个处理方法 中间件 如果没有他 那么就不会执行 usersRoutes
+	ctx.Next()
+}
+
 func main() {
 	app := iris.New()
 
-	mvc.Configure(app.Party("/movies"), movies)
+	// 注册html
+	app.RegisterView(iris.HTML("./web/views/", ".html").Reload(true))
+
+	// 最基本的Get请求
+	app.Get("/ping", func(ctx iris.Context) {
+		_, _ = ctx.WriteString("ping")
+	})
+
+	// 返回.html
+	app.Get("/index", func(ctx iris.Context) {
+		_ = ctx.View("index.html")
+	})
+
+	// 路由的分组
+	userRoute := app.Party("/users", logThisMiddleware)
+	{
+		// 带参数的路由
+		userRoute.Get("/{id:int min(1)}", func(ctx iris.Context) {
+			userId, err := ctx.Params().GetInt("id")
+			fmt.Println(userId, err)
+			_, _ = ctx.WriteString(strconv.Itoa(userId))
+		})
+	}
 
 	err := app.Run(iris.Addr(":8080"))
 	if err != nil {
 		log.Fatal("启动失败: ", err)
 	}
-}
-
-func movies(app *mvc.Application) {
-	resp := repositories.NewMovieRepository()
-	movieService := services.NewMovieService(resp)
-	app.Register(movieService)
-	app.Handle(new(controllers.MovieController))
 }
