140aaa34877f16bd3488a888e10f86fbf87951a8
陈品珍 <549471729@qq.com>
2021-02-03 20:34:18 +0800

    test: json web token

--- web/routes/request.go
+++ web/routes/request.go
@@ -1,10 +1,24 @@
 package routes
 
 import (
+	"github.com/iris-contrib/middleware/jwt"
+	jwtmiddleware "github.com/iris-contrib/middleware/jwt"
 	"github.com/kataras/iris/v12"
 	"iris-test/web/controllers"
 )
 
 func RequestRoute(app *iris.Application) {
 	app.Post("/req", controllers.TestPostJsonHandler)
+
+	jwtRoute := app.Party("/jwt")
+	{
+		jwtHandler := jwtmiddleware.New(jwtmiddleware.Config{
+			ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+				return []byte("My Secret"), nil
+			},
+			SigningMethod: jwt.SigningMethodHS256,
+		})
+		jwtRoute.Use(jwtHandler.Serve)
+		jwtRoute.Get("", controllers.TestJWTHandler)
+	}
 }
